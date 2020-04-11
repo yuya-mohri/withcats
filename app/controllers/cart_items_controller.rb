7f6ab8e71b63ad5a905466f233cde30fb4e9d73a
yuya.mohri <shion0430@gmail.com>
2020-04-11 03:37:30 +0000

    can cart_in

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,8 +1,17 @@
 class CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.order(created_at: :desc)
   end
 
   def create
+    @cart_item = current_user.cart_items.build(cart_item_params)
+    if @cart_item.save
+      flash[:success] = '商品をカートに追加しました'
+      redirect_to  item_url(@cart_item.item_id)
+    else
+      flash.now[:danger] = '商品をカートに追加できませんでした'
+      render 'items/show'
+    end  
   end
 
   def edit
@@ -12,6 +21,10 @@ class CartItemsController < ApplicationController
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    flash[:success] = '商品情報を削除しました'
+    redirect_to cart_items_url
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only[:show, :edit]
+  before_action :require_user_logged_in, only: [:show, :edit]
   
   def show
     @user = User.find(params[:id])
